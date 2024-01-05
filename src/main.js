1f6065099b80b96aa32e5c8fbcfac63d06bef8e4
Grace <gracesilvaduarte@gmail.com>
2024-01-05 18:51:44 -0500

    se añade la animación para la selección del pokémon

--- src/main.js
+++ src/main.js
@@ -1,23 +1,56 @@
-/* seleccion del pokemon inicial */
-function seleccionarPokemon(){
+/* ampliar imagen */
 
-    let inputPikachu = document.getElementById("pikachu");
-    let inputPsyduck = document.getElementById("psyduck");
-    let inputGeodude = document.getElementById("geodude");
+ /*
+listaPokemons[0].onclick =()=>{
+  listaPokemons[0].classList.add("activeImage");
+}
+*/
 
-    if(inputPikachu.checked){
-        alert("haz elegido a pikachu, ten cuidado, le gusta su espacio");
-    }
-        else if(inputPsyduck.checked){
-            alert("psyduck, no lo estreses y se bueno con él")
-        }
-    else{
-        alert("geodude, excelente compañero, entrénalo bien")
-    }
+let listaPokemons = document.querySelectorAll("img");
 
+listaPokemons[0].onclick =()=>{
+  listaPokemons[0].classList.add("activeImage");
+  listaPokemons[1].classList.remove("activeImage");
+  listaPokemons[2].classList.remove("activeImage");
 }
 
-let btnSeleccionarPokemon = document.getElementById("btnSeleccionarPokemon");
+listaPokemons[1].onclick =()=>{
+  listaPokemons[1].classList.add("activeImage");
+  listaPokemons[0].classList.remove("activeImage");
+  listaPokemons[2].classList.remove("activeImage");
+}
 
+listaPokemons[2].onclick =()=>{
+  listaPokemons[2].classList.add("activeImage");
+  listaPokemons[0].classList.remove("activeImage");
+  listaPokemons[1].classList.remove("activeImage");
+}
+
+
+
+/* seleccion del pokemon inicial */
+function seleccionarPokemon() {
+
+  let inputPikachu = document.getElementById("pikachu");
+  let inputPsyduck = document.getElementById("psyduck");
+  let inputGeodude = document.getElementById("geodude");
+
+  if (inputPikachu.checked) {
+    alert("haz elegido a pikachu, ten cuidado, le gusta su espacio");
+  } 
+    else if (inputPsyduck.checked) {
+      alert("psyduck, no lo estreses y se bueno con él");
+  } 
+    else if (inputGeodude.checked) {
+      alert("geodude, excelente compañero, entrénalo bien");
+  }
+}
+
+
+
+let btnSeleccionarPokemon = document.getElementById("btnSeleccionarPokemon");
 btnSeleccionarPokemon.addEventListener("click", seleccionarPokemon);
+
+/* ampliar personaje al hacer click */
+
 
