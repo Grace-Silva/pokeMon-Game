afec85ae8234488e195f430f1a65afbb646719dd
Grace <gracesilvaduarte@gmail.com>
2024-01-12 19:41:48 -0500

    se modifican los mensajes para cada pokemon y se reduce el código

--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
-/* ampliar imagen */
-
-/*
-listaPokemons[0].onclick =()=>{
-  listaPokemons[0].classList.add("activeImage");
-}
-*/
-window.addEventListener("load", ampliarPokemon);
-/* permite que el la imagen del pokemon a seleccionar se amplíe a hacer clic sobre ella.  */
+/* permite que el la imagen del pokemon a seleccionar se amplíe a hacer clic sobre ella.  *
 function ampliarPokemon() {
-
-  document.querySelectorAll("[type='radio']").forEach((x) => x.checked=false);
-
   let listaPokemons = document.querySelectorAll("img");
 
   listaPokemons[0].onclick = () => {
@@ -19,86 +8,72 @@ function ampliarPokemon() {
     listaPokemons[2].classList.remove("activeImage");
   };
 
-  listaPokemons[1].onclick = () => {
-    listaPokemons[1].classList.add("activeImage");
-    listaPokemons[0].classList.remove("activeImage");
-    listaPokemons[2].classList.remove("activeImage");
-  };
+  // y así para los demás personajes....
 
-  listaPokemons[2].onclick = () => {
-    listaPokemons[2].classList.add("activeImage");
-    listaPokemons[0].classList.remove("activeImage");
-    listaPokemons[1].classList.remove("activeImage");
-  };
+}
+*/
+
+// deselecciona todos pokemon para evitar una elección errónea:
+window.addEventListener("load", deseleccionarTodo);
+
+function deseleccionarTodo(){
+  document.querySelectorAll("[type='radio']").forEach((x) => x.checked=false);
 }
 
-/* seleccion del pokemon inicial */
+/* seleccion del pokemon inicial de parte del usuario */
 function seleccionarPokemon() {
+
   let inputPikachu = document.getElementById("pikachu");
   let inputPsyduck = document.getElementById("psyduck");
   let inputGeodude = document.getElementById("geodude");
 
   if (inputPikachu.checked) {
-    alert("haz elegido a pikachu, ten cuidado, le gusta su espacio");
-  } else if (inputPsyduck.checked) {
-    alert("psyduck, no lo estreses y se bueno con él");
-  } else if (inputGeodude.checked) {
-    alert("geodude, excelente compañero, entrénalo bien");
+    alert("ELEGISTE A PIKACHU, LE GUSTA SU ESPACIO ⚡ .");
+  } 
+    else if (inputPsyduck.checked) {
+      alert("ELEGISTE A PSYDUCK. NO LO ESTRESES, SÉ BUENO CON ÉL.");
+  } 
+    else if (inputGeodude.checked) {
+      alert("ELEGISTE A GEODUDE, SU AMISTAD SERÁ COMO LA ROCA.");
   }
   else{
-    alert("Por favor selecciona un PokéMon");
+    alert("Por favor selecciona un PokéMon.");
   }
+  //PC "elige" un PokéMon
   seleccionarPokemonPC();
+  // La página se recarga para seguir jugando
   window.location.reload();
 }
 
 let btnSeleccionarPokemon = document.getElementById("btnSeleccionarPokemon");
 btnSeleccionarPokemon.addEventListener("click", seleccionarPokemon);
 
+//PC "elige" un PokéMon
 function seleccionarPokemonPC(){
 
   let pokemonPC = aleatorio(1,3);
 
   switch (pokemonPC) {
     case 1:
-      alert("PC HA ELEGIDO A BULBASUR");
+      alert("PC HA ELEGIDO A BULBASUR.");
       break;
 
     case 2:
-      alert("PC HA ELEGIDO A CHARMANDER");
+      alert("PC HA ELEGIDO A CHARMANDER.");
       break;
 
     case 3:
-      alert("PC HA ELEGIDO A SQUIRTLE");
+      alert("PC HA ELEGIDO A SQUIRTLE.");
       break;
 
     default:
-      alert("LA PC NO QUIERE JUGAR");
+      alert("LA PC NO QUIERE JUGAR, VUELVE MÁS TARDE.");
       break;
   }
 
 }
 
-/* generar un numero aleatorio entre 1 y 3 */
+/* Se genera un número aleatorio para que la pc seleccione un PokéMon */
 function aleatorio(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-/* ampliar personaje al hacer click */
-
-
-
-/* funcion experimental
-
-document.querySelectorAll("img")
-NodeList(3) [ img.pokemonImage, img.pokemonImage, img.pokemonImage
- ]
-
-for(let i=0; i<3; i++){
-  document.querySelectorAll("img")[i].onclick =()=>{
-    document.querySelectorAll("img")[i].style.border="solid red";
-  }
-}
-function ()
-
-*/
