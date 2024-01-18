bf317aa43d956aea60258e2c77866b3a5b9b8cca
Grace <gracesilvaduarte@gmail.com>
2024-01-17 21:26:10 -0500

    se añade funcionalidad al botón cerrar combate PokéMon

--- src/main.js
+++ src/main.js
@@ -28,34 +28,40 @@ function seleccionarPokemon() {
   let inputPsyduck = document.getElementById("psyduck");
   let inputGeodude = document.getElementById("geodude");
 
+  // pikachu
   if (inputPikachu.checked) {
     alert("ELEGISTE A PIKACHU, LE GUSTA SU ESPACIO ⚡ .");  
     //PC "elige" un PokéMon
     seleccionarPokemonPC();
     mostrarCombate();
   } 
+    // psyduck
     else if (inputPsyduck.checked) {
       alert("ELEGISTE A PSYDUCK. NO LO ESTRESES, SÉ BUENO CON ÉL.");
       //PC "elige" un PokéMon
       seleccionarPokemonPC();
       mostrarCombate();
     } 
+    // geodude
     else if (inputGeodude.checked) {
       alert("ELEGISTE A GEODUDE, SU AMISTAD SERÁ COMO LA ROCA.");
       //PC "elige" un PokéMon
       seleccionarPokemonPC();
       mostrarCombate();
       }
+  // el usuario no elige nada, el PC tampoco    
   else{
     alert("Por favor selecciona un PokéMon.");
+    window.location.reload();
   }
 
-  // La página se recarga para seguir jugando
-  window.location.reload();
+  /* La página se recarga para seguir jugando
+  window.location.reload();*/
 }
 
+// botón para jugar:
 let btnSeleccionarPokemon = document.getElementById("btnSeleccionarPokemon");
-btnSeleccionarPokemon.addEventListener("click", seleccionarPokemon);
+  btnSeleccionarPokemon.addEventListener("click", seleccionarPokemon);
 
 //PC "elige" un PokéMon
 function seleccionarPokemonPC(){
@@ -87,7 +93,25 @@ function aleatorio(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+
 function mostrarCombate(){
+
+  // window background
   let battleFieldContainer = document.querySelector(".battleFieldContainer");
-  battleFieldContainer.style.display = "flex";
+    battleFieldContainer.style.display = "flex";
+
+  // close button
+  let closeFieldButton = document.getElementById("closeFieldButton");
+    closeFieldButton.addEventListener("click", cerrarCombate);
+    
+    let userPokeMon = document.getElementById("userPokeMon");
+    let pcPokeMon = document.getElementById("pcPokeMon");
+
+      // función seleccionar pokemon (user)
+    
+    // cerrar combate PokéMon  y recargar la página para seguir jugando:
+    function cerrarCombate(){
+      battleFieldContainer.style.display = "none";
+      window.location.reload();
+    }
 }
